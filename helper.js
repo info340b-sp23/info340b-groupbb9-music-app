d37e47e07fab71709532104b7b9e5e396efe1c28
mlang2001 <Miles@Miless-MacBook-Pro.local>
2023-05-03 14:14:50 -0700

    added some event listener functionality

--- helper.js
+++ helper.js
@@ -1,36 +1,28 @@
+'use strict';
+
 let loggedIn = false; 
 console.log(loggedIn);
-/*document.getElementById("accountButton").onclick = function () {
-  console.log("clicked");
-  if(loggedIn == true) {
+
+const loginButton = document.getElementById('loginButton');
+if(loginButton) {
+  loginButton.addEventListener('click', () => {
+    console.log('clicked');
+    console.log(loggedIn);
+    loggedIn = true;
+    console.log(loggedIn);
     document.location.href = "profile.html";
-  } else {
-    document.location.href = "login.html";
-  }
+  });
 }
 
-document.getElementById("loginButton").onclick = function () {
-  console.log("clicked");
-  location.href = "profile.html";
-  loggedIn = true;
-  console.log(loggedIn);
-}*/
-
-function accountDirect() {
-  console.log("clicked");
-  console.log(loggedIn);
-  if(loggedIn) {
-    document.location.href = "profile.html";
-  } else {
-    document.location.href = "login.html";
-  }
-}
-
-function login() {
-  console.log("clicked");
-  console.log(loggedIn);
-  loggedIn = true;
-  console.log(loggedIn);
-  document.location.href = "profile.html";
-  
+const accountButton = document.getElementById('accountButton');
+if(accountButton) {
+  accountButton.addEventListener("click", () => {
+    console.log("clicked");
+    console.log(loggedIn);
+    if(loggedIn) {
+      document.location.href = "profile.html";
+    } else {
+      document.location.href = "login.html";
+    }
+  });
 }
