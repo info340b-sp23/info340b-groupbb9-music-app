d0ace738f675b352572923490d5c0f763a3c8c73
Stevi <97136858+stevi2002@users.noreply.github.com>
2023-05-21 18:43:49 -0700

    updated

--- src/App.js
+++ src/App.js
@@ -5,16 +5,16 @@ import { HomePage } from './HomePage.js';
 export default function App(props) {
   return (
     <div>
-         <Login />
-        {/*
-        <UserProfile 
+        {/* <Login /> */}
+        <UserProfile
+          profilePic="../public/img/taylor_swift_eras_tour_1.jpeg" 
           userName="Steven"
           userBio="in my midnight eras~"
           userAnthemURL="https://www.youtube.com/watch?v=lvHZjvIyqsk"
           userAnthem="Maroon"
           favArtist="Taylor Swift"
           favGenre="Pop"
-        />  */}
+        /> 
         {/*<HomePage />*/}
     </div>    
   );
