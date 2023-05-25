10fad02d0c940643ef6d7ad2d1531b33f27c526c
mlang2001 <milesalang@gmail.com>
2023-05-24 20:28:07 -0700

    added routing

--- src/App.js
+++ src/App.js
@@ -3,22 +3,25 @@ import { Login } from './loginPage.js'
 import { UserProfile } from './ProfilePage';
 import HomePage from './HomePage.js';
 import CommentViewPost from './CommentViewPost.js';
+import { Routes, Route, Navigate, Link } from 'react-router-dom';
 
 export default function App(props) {
   return (
     <div>
-        {/*<Login />*/}
-        {/*<UserProfile
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/profile" element={<UserProfile 
           profilePic="../public/img/taylor_swift_eras_tour_1.jpeg" 
           userName="Steven"
           userBio="in my midnight eras~"
           userAnthemURL="https://www.youtube.com/watch?v=lvHZjvIyqsk"
           userAnthem="Maroon"
           favArtist="Taylor Swift"
-          favGenre="Pop"
-        />*/}
-        <HomePage />
-        {/*<CommentViewPost postId={2} /> */}
+          favGenre="Pop"  />} />
+        <Route path="/comment" element={<CommentViewPost postId={2} />} />
+        <Route path="*" element={<Navigate to="/home"/>} />
+      </Routes>
     </div>    
   );
 }
