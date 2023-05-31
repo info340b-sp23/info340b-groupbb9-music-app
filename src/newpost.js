2528925569a6fe5eb1882d336d6fc115d4cf012e
WenChiC2024 <91912629+WenChiC2024@users.noreply.github.com>
2023-05-31 13:03:41 -0700

    Update newpost.js with captue url thumbnail

--- src/newpost.js
+++ src/newpost.js
@@ -1,7 +1,41 @@
 import React from 'react';
 
 class NewPost extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: '',
+      artist: '',
+      url: '',
+      thumbnail: ''
+    };
+  }
+
+  handleChange = (event) => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+    if (name === 'url') {
+      const videoId = this.extractVideoId(value);
+      if (videoId) {
+        this.setState({ thumbnail: `https://img.youtube.com/vi/${videoId}/hqdefault.jpg` });
+      }
+    }
+  };
+
+  extractVideoId = (url) => {
+    const match = url.match(/youtube\.com\/watch\?v=([^\&\?\/]+)/);
+    return match && match[1];
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('Form submitted!');
+    // Add your form submission logic here
+  };
+
   render() {
+    const { title, artist, url, thumbnail } = this.state;
+
     return (
       <div>
         <nav>
@@ -14,13 +48,15 @@ class NewPost extends React.Component {
         <h1 className="newpost_heading">Add New Post</h1>
         <form className="new_post_form" onSubmit={this.handleSubmit}>
           <label htmlFor="title">Title:</label>
-          <input type="text" id="title" name="title" required /><br />
+          <input type="text" id="title" name="title" value={title} onChange={this.handleChange} required /><br />
 
           <label htmlFor="artist">Artist:</label>
-          <input type="text" id="artist" name="artist" required /><br />
+          <input type="text" id="artist" name="artist" value={artist} onChange={this.handleChange} required /><br />
 
           <label htmlFor="url">URL:</label>
-          <input type="text" id="url" name="url" required /><br />
+          <input type="text" id="url" name="url" value={url} onChange={this.handleChange} required /><br />
+
+          <img src={thumbnail} alt="thumbnail" /><br />
 
           <input type="submit" value="Submit" />
         </form>
@@ -30,13 +66,7 @@ class NewPost extends React.Component {
 
   accountDirect() {
     console.log('Account button clicked!');
-    // logic
-  }
-
-  handleSubmit(event) {
-    event.preventDefault();
-    console.log('Form submitted!');
-    // submission logic
+    // Add logic
   }
 }
 
