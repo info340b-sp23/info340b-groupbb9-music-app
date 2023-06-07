c8fe14c696e13338f92f7d0b4d58cfdd7e9c07fa
Ryotaro Hayashi <92127242+ryo884-2177391@users.noreply.github.com>
2023-06-06 18:46:02 -0700

    Update HomePage.js

--- src/HomePage.js
+++ src/HomePage.js
@@ -2,7 +2,6 @@ import React, { UseState } from "react";
 import { NavBar } from './Navigation.js';
 import { Posts } from './Posts.js';
 import { Footer } from './Footer.js';
-import CommentViewPost from './CommentViewPost.js';
 
 export default function HomePage() {
   //const [selectedPostId, setSelectedPostId] = UseState(null);
