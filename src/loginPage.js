55202195b44d51c518c4c4bf42f388fea32e9ab2
mlang2001 <milesalang@gmail.com>
2023-06-06 21:56:10 -0700

    fixed login page styling

--- src/loginPage.js
+++ src/loginPage.js
@@ -3,22 +3,24 @@ import './index.css';
 import { Footer } from "./Footer";
 export function Login() {
   return (
-    <main>
-        <div className="container">
-          <header>
-            <h1>compose your social symphony~🎶✨</h1>
-          </header>
-          <form action="#" method="post" className="login_form">
-            <label htmlFor="username">Username:</label>
-            <input className="login_reactive_field" type="text" id="username" name="username" />
+    <main id="login_body">
+      <body>
+        <div className="container" id="login_container">
+          <h1 id="login_heading">compose your social symphony</h1>
+          <div>
+            <form action="#" method="post" className="login_form">
+              <label htmlFor="username">Username:</label>
+              <input className="login_reactive_field" type="text" id="username" name="username" />
 
-            <label htmlFor="password">Password:</label>
-            <input className="login_reactive_field" type="password" id="password" name="password" />
+              <label htmlFor="password">Password:</label>
+              <input className="login_reactive_field" type="password" id="password" name="password" />
 
-            <button type="button" className="login_reactive_field" id="loginButton" onClick={Login}>Login</button>
-          </form>
-          <Footer />
+              <button type="button" className="login_reactive_field" id="loginButton" onClick={Login}>Login</button>
+            </form>
+          </div>
         </div>
+      </body>
+      <Footer id="login_footer"/>
     </main>
   );
 }
