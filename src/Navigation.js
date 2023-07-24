6c90305ad7a15e302c79295110b3f9e77cf6af1f
Ryotaro Hayashi <92127242+ryo884-2177391@users.noreply.github.com>
2023-07-23 18:26:41 -0700

    Update Navigation.js
    commit

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,19 +1,18 @@
-import React from 'react';
 import { NavLink } from 'react-router-dom';
-
 export function NavBar() {
-    return (
-        <nav>
-            <div className="nav-left">
-            <span aria-label="Home"><NavLink to="/home"><img src="img/icon.png" alt="logo"></img></NavLink></span>
-            <a href="index.html"></a>
-            </div>
-
-            <div className="nav_right">
-                <NavLink to="/home">Home</NavLink>
-                <NavLink to="/create-post">Create Post</NavLink>
-                <NavLink to="/about">About</NavLink>
-            </div>
-        </nav>
-    );
+  return (
+    <nav>
+      <div className='nav-left'>
+        <span aria-label="Home"><NavLink to="/home"><img src="img/beatbuds.png" alt="logo"></img></NavLink></span>
+        <a href="index.html"></a>
+      </div>
+      <div className='nav-center'>
+        <span aria-label="Home"><NavLink to="/home"><h1 className='header-title'>BEATBUDS</h1></NavLink></span>
+      </div>
+      <div className="nav-right">
+        <NavLink to="/newPost">New Post</NavLink>
+        <NavLink to="/profile"><img src="img/default_account_logo.png" alt="Login"></img></NavLink>
+      </div>
+    </nav>
+  );
 }
