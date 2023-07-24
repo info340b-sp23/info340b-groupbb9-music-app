ef7181466ce7eb1a8792fb1c878241f9834cd32d
Ryotaro Hayashi <92127242+ryo884-2177391@users.noreply.github.com>
2023-07-23 18:21:49 -0700

    Update Navigation.js
    Second commit to NavBar

--- src/Navigation.js
+++ src/Navigation.js
@@ -2,20 +2,18 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 export function NavBar() {
-  return (
-    <nav>
-      <div className='nav-left'>
-        <span aria-label="Home"><NavLink to="/home"><img src="img/beatbuds.png" alt="logo"></img></NavLink></span>
-        <a href="index.html"></a>
-      </div>
-      <div className='nav-center'>
-        <span aria-label="Home"><NavLink to="/home"><h1 className='header-title'>BEATBUDS</h1></NavLink></span>
+    return (
+        <nav>
+            <div className="nav-left">
+            <span aria-label="Home"><NavLink to="/home"><img src="img/icon.png" alt="logo"></img></NavLink></span>
+            <a href="index.html"></a>
+            </div>
 
-      </div>
-      <div className="nav-right">
-        <NavLink to="/newPost">New Post</NavLink>
-        <NavLink to="/profile"><img src="img/default_account_logo.png" alt="Login"></img></NavLink>
-      </div>
-    </nav>
-  );
+            <div className="nav_right">
+                <NavLink to="/home">Home</NavLink>
+                <NavLink to="/create-post">Create Post</NavLink>
+                <NavLink to="/about">About</NavLink>
+            </div>
+        </nav>
+    );
 }
